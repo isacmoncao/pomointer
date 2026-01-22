e3b731a690c2a4e3156790159a5e38bfd3f52f47
José Isac <joseisac@tutamail.com>
2026-01-22 14:06:21 -0300

    refactor(hashmap.c): simplifies header usage

--- src/hashmap.c
+++ src/hashmap.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "../include/hashmap.h"
+#include "hashmap.h"
 
 // ------------------- AUXILIARY FUNCTIONS -------------------------------
 
