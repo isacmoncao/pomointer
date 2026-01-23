62fa65fa9e1c87612e07fafbaa04e00d20f4369a
José Isac <joseisac@tutamail.com>
2026-01-23 03:17:25 -0300

    util.h: better organize the functions declarations

--- include/util.h
+++ include/util.h
@@ -2,15 +2,26 @@
 #define POMOINTER_UTIL_H
 
 #include <stdbool.h>
+#include <time.h>
 
-char** split_string(const char* str, const char delimiter, int* count);
-void free_string_array(char** array);
+// Strings
 int count_stars(const char* str);
+char** split_string(const char* str, const char delimiter, int* count);
 char* strip_string(const char* str, const char* chars);
-char* int_to_string(int n);
+void free_string_array(char** array);
+
+// Conversions
 int string_to_int(const char* str);
+char* int_to_string(int n);
+time_t string_to_time(const char* str);
+char* time_to_string(time_t time);
+
+// Booleans
 bool is_empty_str(const char* str);
 bool is_comment(const char* str);
 
+// Files
+time_t get_file_mod_date(const char* path);
+void print_file(const char* path);
 
 #endif
